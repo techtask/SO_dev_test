e2de2fdf995842bedda85ef389fb9b573cb6c2f1
techtask <techtask@mailinator.com>
2016-12-14 20:42:50 -0500

    Fix missing configuration argument.

--- src/Config.php
+++ src/Config.php
@@ -7,12 +7,14 @@ class Config
     protected $username;
     protected $dsn = 'pgsql:host=localhost;dbname=silverorange_dev_test;';
     protected $paths = array();
+    protected $commandList = array();
 
-    public function __construct($username, array $paths)
+    public function __construct($username, Array $paths, Array $commandList)
     {
         $this->username = $username;
         $this->dsn = $this->dsn . 'user=' . $this->username;
         $this->paths = $paths;
+        $this->commandList = $commandList;
     }
 
     public function getDSN()
@@ -27,7 +29,12 @@ class Config
 
     public function getDefaultPath()
     {
-        // FIXME This is hardcoded.. No schema validation for paths.
-    return $this->paths["default"];
+        // FIXME This is hardcoded.. No schema validation for paths, or the rest of this config...
+        return $this->paths["default"];
+    }
+
+    public function getCommandList()
+    {
+        return $this->commandList;
     }
 }
